0294d852e63ef774c955e2abb94c1a701a178cdf
mdev <mukeshdevnp@gmail.com>
2017-06-10 19:36:53 +0545

    Help Text  Added

---
+++ Assets/scripts/HelpTextManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpTextManager : MonoBehaviour
+{
+    public GameObject HelpText;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(ExecuteAfterTime(2));
+	}
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        HelpText.SetActive(false);
+    }
+}
